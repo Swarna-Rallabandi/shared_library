19bc95c9197efac48c7abf6439342145faa95ae4
Swarna Rallabandi <swarna.rallabandi@gmail.com>
2026-01-08 07:38:27 +1100

    added library code

---
+++ vars/firstpipeline.groovy
@@ -0,0 +1,44 @@
+import comp/i27academy/builds.Calculator
+
+def call(Map pipelineParams){
+Calculator calculator = new Calculator(this)
+
+pipeline {
+    agent {
+        label 'slave1'
+    }
+    environment {
+        APPLICATION_NAME = ${pipelineParams.appName}
+    }
+    stages {
+        stage ('Addition') {
+            steps {
+                script {
+                    echo "calling add method"
+               println calculator.add(2,3)
+            }
+                }
+
+        }
+        
+        stage ('Build') {
+              steps{
+                echo "buildig the ${env.APPLICATION_NAME} applicationa"
+            }
+        }
+        
+        }
+        stage ('Test'){
+            steps {
+                echo "testing the ${env.APPLICATION_NAME} application"
+            }
+        }
+        stage ('Deploy to dev'){
+            steps {
+                    echo "dev ${env.APPLICATION_NAME} deploy"
+            }
+       
+    }
+}
+
+}
